466fcc694905a3aae791b102264f8c05cc1bd6dd
Jitesh Vyas <jitesh.vyas@outlook.com>
2020-01-11 16:50:40 +0530

    Added solution - generate shortener url.

---
+++ ShortenerURL/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ShortenerURL
+{
+    class Program
+    {
+        //C# console app that generates a URL shortener. 
+        //Any URL given to it as input will return an unique shortened URL. 
+        //Something like bitly or tiny.cc
+
+        static void Main(string[] args)
+        {
+            char[] url = "www.amazon.com/home/index".ToCharArray();
+
+            int limit = 12345;
+            String shorturl = UrlShortener.GetShortUrl(url,limit);
+            Console.WriteLine("Generated short url is " + shorturl);
+            Console.Read();
+        }
+    }
+}
---
+++ ShortenerURL/UrlShortener.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace ShortenerURL
+{
+    public class UrlShortener
+    {
+
+        public static string GetShortUrl(char[] url, int counter)
+        {
+            StringBuilder shortUrl = new StringBuilder();
+            int mod = url.Length;
+            while (counter > 0)
+            {
+                shortUrl.Append(url[counter % mod]);
+                counter = counter / mod;
+            }
+
+            return shortUrl.ToString();
+        }
+    }
+
+}
